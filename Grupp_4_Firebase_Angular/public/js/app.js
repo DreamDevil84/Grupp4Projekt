c4c4c688bfe1d9fdc3e7fb8b72baaea79e3a01dc
DreamDevil84 <daniel.edstrom1984@hotmail.com>
2017-05-09 18:18:29 +0200

    Add files via upload

---
+++ Grupp_4_Firebase_Angular/public/js/app.js
@@ -0,0 +1,50 @@
+(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB3UdUl1As-W_3gHCf-aDadJw0myIOvdR8",
+        authDomain: "schoolweb-35754.firebaseapp.com",
+        databaseURL: "https://schoolweb-35754.firebaseio.com",
+        projectId: "schoolweb-35754",
+        storageBucket: "schoolweb-35754.appspot.com",
+        messagingSenderId: "387758932838"
+    };
+    firebase.initializeApp(config);
+
+    angular
+        .module('app', ['firebase'])
+        .controller('MyCtrl', function ($scope, $firebaseObject, $firebaseArray, $firebaseAuth) {
+            var ref = firebase.database().ref().child('data');
+            var syncObject = $firebaseObject(ref);
+            syncObject.$bindTo($scope, 'data');
+
+            var msgRef = firebase.database().ref().child('messages');
+            $scope.messages = $firebaseArray(msgRef);
+            $scope.addMessage = function () {
+                $scope.messages.$add({
+                    text: $scope.newMessageText
+                });
+            };
+
+            var auth = $firebaseAuth();
+
+            $scope.email = 'daniel.edstrom1984@hotmail.com';
+            $scope.password = 'newton123';
+
+            $scope.signIn = function (email, password) {
+                $scope.firebaseUser = null;
+                $scope.error = null;
+
+                auth.$signInWithEmailAndPassword(email, password).then(function (firebaseUser) {
+                    $scope.firebaseUser = firebaseUser;
+                })
+                    .catch(function (error) {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        $scope.error = error;
+                    });
+            };
+        });
+
+
+}());
