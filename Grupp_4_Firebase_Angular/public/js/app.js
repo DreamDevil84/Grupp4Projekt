9150abe7f0fb0675a84cb62132e13fde9130ad7f
DreamDevil84 <daniel.edstrom1984@hotmail.com>
2017-05-11 09:45:59 +0200

    Add files via upload

--- Grupp_4_Firebase_Angular/public/js/app.js
+++ Grupp_4_Firebase_Angular/public/js/app.js
@@ -12,48 +12,124 @@
     firebase.initializeApp(config);
 
     angular
-        .module('app', ['firebase'])
-        .factory('Auth', function($firebaseAuth){
+        .module('app', ['firebase', 'ngRoute'])
+        .factory('Auth', function ($firebaseAuth) {
             return $firebaseAuth();
         })
-        .controller('MyCtrl', function ($scope, Auth, $firebaseObject, $firebaseArray, $firebaseAuth) {
-            // var ref = firebase.database().ref().child('data');
-            // var syncObject = $firebaseObject(ref);
-            // syncObject.$bindTo($scope, 'data');
-
-            // var msgRef = firebase.database().ref().child('messages');
-            // $scope.messages = $firebaseArray(msgRef);
-            // $scope.addMessage = function () {
-            //     $scope.messages.$add({
-            //         text: $scope.newMessageText
-            //     });
-            // };
-
+        .controller('LoginCtrl', function ($scope, Auth, $location) {
             $scope.auth = Auth;
 
-            $scope.auth.$onAuthStateChanged(function(firebaseUser){
+            $scope.auth.$onAuthStateChanged(function (firebaseUser) {
                 $scope.firebaseUser = firebaseUser;
+                if($scope.firebaseUser === null)
+                {
+                    $location.path('/home');
+                }
+                else
+                {
+                    $location.path('/admin');
+                }
             });
+            $scope.email = 'daniel.edstrom1984@hotmail.com';
+            $scope.password = 'newton123';
 
-            // var auth = $firebaseAuth();
+        })
+        //denna function är till för att se om användaren inte är autensierad och blir då sparkad till förstasidan
+        .run(function ($rootScope, $location) {
+            $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+                if (error === 'AUTH_REQUIRED') {
+                    $location.path('/home');
+                }
+            });
+        })
+        .config(function ($routeProvider) {
+            $routeProvider.when(
+                '/home',
+                {
+                    controller: 'HomeCtrl',
+                    templateUrl: 'views/home/home.html',
+                    resolve:
+                    {
+                        'currentAuth': function (Auth) {
+                            return Auth.$waitForSignIn();
+                        }
+                    }
+                }).when(
+                '/admin',
+                {
+                    controller: 'AdminCtrl',
+                    templateUrl: 'views/admin/home.html',
+                    resolve:
+                    {
+                        'currentAuth': function (Auth) {
+                            return Auth.$requireSignIn();
+                        }
+                    }
+                });
+        })
+        .controller('HomeCtrl', function (currentAuth) {
 
-             $scope.email = 'daniel.edstrom1984@hotmail.com';
-             $scope.password = 'newton123';
+        })
+        .controller('AdminCtrl', function (currentAuth) {
 
-            // $scope.signIn = function (email, password) {
-            //     $scope.firebaseUser = null;
-            //     $scope.error = null;
-
-            //     auth.$signInWithEmailAndPassword(email, password).then(function (firebaseUser) {
-            //         $scope.firebaseUser = firebaseUser;
-            //     })
-            //         .catch(function (error) {
-            //             var errorCode = error.code;
-            //             var errorMessage = error.message;
-            //             $scope.error = error;
-            //         });
-            // };
         });
+
+
+
+
+    // .directive('startContent', function(){
+    //     return {
+    //         restrict: 'E',
+    //         templateUrl: 'views/home/home.html'
+    //     };
+    // })
+    // .directive('adminContent', function(){
+    //     return {
+    //         restrict: 'E',
+    //         templateUrl: 'views/admin/home.html'
+    //     };
+    // })
+    // .directive('teacherContent', function(){
+    //     return {
+    //         restrict: 'E',
+    //         templateUrl: 'views/teacher/home.html'
+    //     };
+    // })
+    // .directive('studentContent', function(){
+    //     return {
+    //         restrict: 'E',
+    //         templateUrl: 'views/student/home.html'
+    //     };
+    // });
+
+
+
+    // var auth = $firebaseAuth();
+    // $scope.signIn = function (email, password) {
+    //     $scope.firebaseUser = null;
+    //     $scope.error = null;
+
+    //     auth.$signInWithEmailAndPassword(email, password).then(function (firebaseUser) {
+    //         $scope.firebaseUser = firebaseUser;
+    //     })
+    //         .catch(function (error) {
+    //             var errorCode = error.code;
+    //             var errorMessage = error.message;
+    //             $scope.error = error;
+    //         });
+    // };
+    // var ref = firebase.database().ref().child('data');
+    // var syncObject = $firebaseObject(ref);
+    // syncObject.$bindTo($scope, 'data');
+
+    // var msgRef = firebase.database().ref().child('messages');
+    // $scope.messages = $firebaseArray(msgRef);
+    // $scope.addMessage = function () {
+    //     $scope.messages.$add({
+    //         text: $scope.newMessageText
+    //     });
+    // };
+
 
 
 }());
