97c7f333e7daeb56a81a55b6a6add9bebd30a652
sSsebbe <sebastian.nilsson@live.com>
2017-05-18 12:40:29 +0200

    androidappen
    uppdaterad

---
+++ MyApplication/app/src/main/java/bettercallnilsson/com/androidapp1/PresenceActivity.java
@@ -0,0 +1,14 @@
+package bettercallnilsson.com.androidapp1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presence);
+    }
+
+}
